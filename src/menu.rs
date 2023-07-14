ad8a60b6b90bb5bc256546314f8b1b2d4c744782
Amir Grozki <amirgrozki@gmail.com>
2023-07-15 01:07:23 +0300

    Added Ctrl+O shortcut to opening a ROM.

--- src/menu.rs
+++ src/menu.rs
@@ -1,4 +1,8 @@
-﻿use tao::menu::{MenuBar, MenuId, MenuItem, MenuItemAttributes};
+﻿use tao::{
+    keyboard::{KeyCode, ModifiersState},
+    accelerator::Accelerator,
+    menu::{MenuBar, MenuId, MenuItem, MenuItemAttributes}
+};
 
 pub(crate) const MENU_OPEN: MenuId = MenuId(1);
 
@@ -6,7 +10,9 @@ pub(crate) fn build_menu() -> MenuBar {
     let mut root = MenuBar::new();
     let mut file_menu = MenuBar::new();
 
-    file_menu.add_item(MenuItemAttributes::new("&Open ROM file...").with_id(MENU_OPEN));
+    file_menu.add_item(MenuItemAttributes::new("&Open ROM file...")
+        .with_id(MENU_OPEN)
+        .with_accelerators(&Accelerator::new(Some(ModifiersState::CONTROL), KeyCode::KeyO)));
     file_menu.add_native_item(MenuItem::Separator);
     file_menu.add_native_item(MenuItem::Quit);
     root.add_submenu("&File", true, file_menu);
